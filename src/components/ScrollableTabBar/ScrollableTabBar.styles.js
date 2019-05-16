9fd271a616f243971cf3dfa2553a2199c3ea2be7
Paweł Karniej <karniej.p@gmail.com>
2019-05-16 11:32:52 +0200

    Added ScrollableTabView

---
+++ src/components/ScrollableTabBar/ScrollableTabBar.styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    width: '100%',
+    zIndex: 10
+  },
+  tabWrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingBottom: 4
+  },
+  countWrapper: {
+    width: 14,
+    height: 14,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
