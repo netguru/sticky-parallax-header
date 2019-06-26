ede4842210824a0164bf4e63bf7121eae66e1df0
Paweł Karniej <karniej.p@gmail.com>
2019-06-27 00:16:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { arrayOf, bool, func, node, number, shape, string } from 'prop-types'
-import { Dimensions, ImageBackground, ScrollView, View, Animated, FlatList } from 'react-native'
+import { Dimensions, ImageBackground, ScrollView, View, Animated } from 'react-native'
 import { ScrollableTabBar, ScrollableTabView } from './components'
 import styles from './styles'
 
-const { divide, Value, createAnimatedComponent, event, spring, timing, sequence } = Animated
+const { divide, Value, createAnimatedComponent, event } = Animated
 const AnimatedScrollView = createAnimatedComponent(ScrollView)
 
 class StickyParalaxHeader extends Component {
@@ -15,6 +15,7 @@ class StickyParalaxHeader extends Component {
     const scrollXIOS = new Value(initialPage * width)
     const containerWidthAnimatedValue = new Value(width)
 
+    // eslint-disable-next-line no-underscore-dangle
     containerWidthAnimatedValue.__makeNative()
     const scrollValue = divide(scrollXIOS, containerWidthAnimatedValue)
     this.state = {
@@ -35,10 +36,10 @@ class StickyParalaxHeader extends Component {
     this.scrollY.removeListener()
   }
 
-  onScrollEndSnapToEdge = (event, scrollHeight) => {
+  onScrollEndSnapToEdge = (ev, scrollHeight) => {
     const { snapToEdge } = this.props
     const scrollNode = this.scroll.getNode()
-    const { y } = event.nativeEvent.contentOffset
+    const { y } = ev.nativeEvent.contentOffset
 
     if (y < -20) this.spring(0, y)
 
@@ -280,7 +281,7 @@ class StickyParalaxHeader extends Component {
           ref={(c) => {
             this.scroll = c
           }}
-          onScrollEndDrag={event => this.onScrollEndSnapToEdge(event, scrollHeight)}
+          onScrollEndDrag={ev => this.onScrollEndSnapToEdge(ev, scrollHeight)}
           scrollEventThrottle={16}
           stickyHeaderIndices={shouldRenderTabs ? [1] : []}
           showsVerticalScrollIndicator={false}
@@ -296,8 +297,8 @@ class StickyParalaxHeader extends Component {
             ],
             {
               useNativeDriver: true,
-              listener: (event) => {
-                this.isCloseToBottom(event.nativeEvent)
+              listener: (ev) => {
+                this.isCloseToBottom(ev.nativeEvent)
                 scrollEvent(event)
               }
             }
