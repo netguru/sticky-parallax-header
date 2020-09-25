8b025737a0955143b80cb36997588a45bee514e4
Piotr Pietruszewski <piotr.pietruszewski@netguru.com>
2020-09-25 12:47:27 +0200

    fix: added responsive font size helper

--- examples/expo/src/constants/screenStyles.js
+++ examples/expo/src/constants/screenStyles.js
@@ -58,8 +58,8 @@ const screenStyles = StyleSheet.create({
   },
   message: {
     color: colors.white,
-    fontSize: constants.responsiveWidth(11),
-    lineHeight: constants.responsiveWidth(11) * 1.2,
+    fontSize: constants.getResponsiveFontSize(32),
+    lineHeight: constants.getResponsiveFontSize(32) * 1.2,
     letterSpacing: -1,
     fontFamily: 'AvertaStd-Semibold',
   },
