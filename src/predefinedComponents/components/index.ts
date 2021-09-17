7cd7022dceda16a8b8cea07ec6a6df291453ee65
Piotr Pietruszewski <piotr.pietruszewski@netguru.com>
2021-09-17 14:31:03 +0200

    refactor: removed unused components and assets

--- src/predefinedComponents/components/index.ts
+++
@@ -1,6 +0,0 @@
-import QuizListElement from './QuizListElement/QuizListElement';
-import UserModal from './UserModal/UserModal';
-import QuizCard from './QuizCard/QuizCard';
-import QuizOption from './QuizOption/QuizOption';
-
-export { QuizListElement, UserModal, QuizCard, QuizOption };
