0c6fba8f8d84f856ccc902656c240abb0f2aaf77
tomaszrej <tomasz.rej@netguru.com>
2021-10-04 13:49:01 +0200

    feat(docs): create landing page for docs

---
+++ docs/src/data/sections.js
@@ -0,0 +1,20 @@
+export const SECTIONS_DATA = [
+  {
+    id: 1,
+    title: 'Easy to use',
+    description: 'Three predefined components included and the possibility to create a fully custom header.',
+    imageName: 'bubbles'
+  },
+  {
+    id: 2,
+    title: 'Type safe',
+    description: 'Written in TypeScript and therefore compatibile with projects created in JavaScript and TypeScript.',
+    imageName: 'office-98'
+  },
+  {
+    id: 3,
+    title: 'Simply and lightwight',
+    description: 'The code is simple, has minimum dependencies and doesn’t include any native code.',
+    imageName: 'elements'
+  }
+];
