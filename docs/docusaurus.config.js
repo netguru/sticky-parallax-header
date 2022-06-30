bb9b1347eb4b38a64a64dde8e650ea0f081086a4
Mateusz Mędrek <mateusz.medrek@netguru.com>
2022-06-30 18:02:11 +0200

    fix: docs trailing slash

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'React Native Sticky Parallax Header',
   tagline:
     'A simple React Native library, enabling to create a fully custom header for your iOS and Android apps.',
-  url: 'https://netguru.github.io/',
+  url: 'https://netguru.github.io',
   baseUrl: '/sticky-parallax-header/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
